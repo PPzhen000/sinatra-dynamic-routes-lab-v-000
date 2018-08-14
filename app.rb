6f466ea7d766a2020383b38c5f76c050a0642861
Ariel Zhen <zhenpingping@live.com>
2018-08-14 05:07:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,28 +24,24 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}." 
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
-    @num1 = params[:number1].to_i 
-    @num2 = params[:number2].to_i 
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
 
-    if @operation == "add" 
-      result = @num1 + @num2 
-      "#{result}"
+    if @operation == "add"
+      result = @num1 + @num2
     elsif @operation == "subtract"
-      result = @num1 - @num2 
-      "#{result}"
+      result = @num1 - @num2
     elsif @operation == "multiply"
-      result = @num1 * @num2 
-      "#{result}"
+      result = @num1 * @num2
     elsif @operation == "divide"
       result = @num1 / @num2 
-      "#{result}"
-    end   
-      
-  end
+    end
 
+  end
+  "#{result}"
 end
