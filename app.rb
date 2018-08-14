a4240e0c68ac25d234bd34531e4d5852cdbbc343
Ariel Zhen <zhenpingping@live.com>
2018-08-14 04:52:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,24 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/:name' do 
+  get '/reversename/:name' do
+    params[:name].reverse 
+  end
 
-  end 
+  get '/square/:number' do
 
-  get '/square/:number' do 
+  end
 
-  end 
+  get '/say/:number/:phrase' do
 
-  get '/say/:number/:phrase' do 
+  end
 
-  end 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+  end
 
-  end 
+  get '/:operation/:number1/:number2' do
 
-  get '/:operation/:number1/:number2' do 
+  end
 
-  end 
-  
 end
